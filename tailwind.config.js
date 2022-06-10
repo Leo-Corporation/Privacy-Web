9844b21ff96f0cff42c7916541070fbd9f58ec88
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-06-10 15:41:12 +0200

    Added 404 page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'move-bg': {
+          to: {
+            backgroundPosition: '400% 0',
+          },
+        },
+      },
+      animation: {
+        'move-bg': 'move-bg 8s infinite linear',
+        'move-bg-l': 'move-bg 16s infinite linear',
+      },
+    },
   },
   plugins: [],
 }
